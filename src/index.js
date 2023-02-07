5d3772d77a233cc7697e23ce72a17c82e1a26dce
TAMA983472 <124493249+TAMA983472@users.noreply.github.com>
2023-02-07 09:41:44 +0900

    [学習]テンプレート文字列の利用

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import "./styles.css";
+var val1 = "aaa";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+console.log(val1);
+
+const name = "aaa";
+const age = "28";
+
+//テンプレート文字列の利用
+const templateTest = `私の名前は${name}です。年齢は${age}です。`;
+console.log(templateTest);
